bbb451fae7d08c8e86d532180458085409b8a0a0
RP <RP@mail.com>
2023-02-07 20:01:13 +0100

    a hozzáadás múködik

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 
 const dolgozoTorzs = document.querySelector("#dolgozoTorzs");
 
+const nameElem = document.querySelector("#name");
+const cityElem = document.querySelector("#city");
+const salaryElem = document.querySelector("#salary");
+
+const addButton = document.querySelector("#addButton");
+
 const dolgozoLista = [
     {name: "Pali", city: "Szolnok", salary: 385},
     {name: "Pisti", city: "Csolnok", salary: 386},
@@ -11,24 +17,52 @@ const dolgozoLista = [
     {name: "Panni", city: "Tata", salary: 600},
 ];
 
-dolgozoLista.forEach((dolgozo) => {
-    console.log(dolgozo.city);
+function loadEmployee(){
+    dolgozoLista.forEach((dolgozo) => {
 
-    let tr = document.createElement('tr');
-    let tdName = document.createElement('td');
-    let tdCity = document.createElement('td');
-    let tdSalary = document.createElement('td');
-
-    tdName.textContent = dolgozo.name;
-    dolgozoTorzs.append(tr);
-    tr.append(tdName);
-
-    tdCity.textContent = dolgozo.city;
-    dolgozoTorzs.append(tr);
-    tr.append(tdCity);
-
-    tdSalary.textContent = dolgozo.salary;
-    dolgozoTorzs.append(tr);
-    tr.append(tdSalary);
+        let tr = document.createElement('tr');
+        let tdName = document.createElement('td');
+        let tdCity = document.createElement('td');
+        let tdSalary = document.createElement('td');
     
+        tdName.textContent = dolgozo.name;
+        dolgozoTorzs.append(tr);
+        tr.append(tdName);
+    
+        tdCity.textContent = dolgozo.city;
+        dolgozoTorzs.append(tr);
+        tr.append(tdCity);
+    
+        tdSalary.textContent = dolgozo.salary;
+        dolgozoTorzs.append(tr);
+        tr.append(tdSalary);
+        
+    });
+};
+
+loadEmployee();
+
+addButton.addEventListener('click', () =>{
+    console.log("oké");
+    addEmployee();
 });
+
+function addEmployee(){
+    
+    dolgozo = {
+        name: nameElem.value,
+        city: cityElem.value,
+        salary: salaryElem.value
+    };
+    dolgozoLista.push(dolgozo);
+    console.log(dolgozoLista);
+    clearFields();
+    dolgozoTorzs.textContent = '';
+    loadEmployee();
+}
+
+function clearFields(){
+    nameElem.value = '';
+    cityElem.value = '';
+    salaryElem.value = '';
+}
